df620ea17c4b1cbab2de56ebf561e01fdcae0965
Ilia Kaziamov <ilia@LAPTOP-FMM9O2JE.localdomain>
2022-07-28 16:40:44 +0300

    New file

---
+++ tests/test_capitalize.py
@@ -0,0 +1,19 @@
+from scr.capitalize import capitalize
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
+
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
+
+if capitalize(' ') != ' ':
+    raise Exception('Функция работает неверно!')
+
+if capitalize('Hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
+
+print('Все тесты пройдены!')
+
+
+
+
